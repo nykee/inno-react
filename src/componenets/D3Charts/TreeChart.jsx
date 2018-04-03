1c8a8752bd99f2e4103140092317fc7b0d41e63f
nykee <nykee@sina.com>
2018-04-03 15:09:18 +0800

    公司名称替换为XY

--- src/componenets/D3Charts/TreeChart.jsx
+++ src/componenets/D3Charts/TreeChart.jsx
@@ -17,6 +17,71 @@ class TreeChart extends React.Component {
             .size([width,height-200])
             .separation(function(a, b) { return (a.parent === b.parent ? 1 : 2); });
 
+        let CityTreedata= "{\n" +
+            "            \"name\":\"中国\",\n" +
+            "            \"children\":\n" +
+            "                [\n" +
+            "                    {\n" +
+            "                        \"name\":\"浙江\" ,\n" +
+            "                        \"children\":\n" +
+            "                            [\n" +
+            "                                {\"name\":\"杭州\" },\n" +
+            "                                {\"name\":\"宁波\" },\n" +
+            "                                {\"name\":\"温州\" },\n" +
+            "                                {\"name\":\"绍兴\" }\n" +
+            "                            ]\n" +
+            "                    },\n" +
+            "\n" +
+            "                    {\n" +
+            "                        \"name\":\"广西\" ,\n" +
+            "                        \"children\":\n" +
+            "                            [\n" +
+            "                                {\n" +
+            "                                    \"name\":\"桂林\",\n" +
+            "                                    \"children\":\n" +
+            "                                        [\n" +
+            "                                            {\"name\":\"秀峰区\"},\n" +
+            "                                            {\"name\":\"叠彩区\"},\n" +
+            "                                            {\"name\":\"象山区\"},\n" +
+            "                                            {\"name\":\"七星区\"}\n" +
+            "                                        ]\n" +
+            "                                },\n" +
+            "                                {\"name\":\"南宁\"},\n" +
+            "                                {\"name\":\"柳州\"},\n" +
+            "                                {\"name\":\"防城港\"}\n" +
+            "                            ]\n" +
+            "                    },\n" +
+            "\n" +
+            "                    {\n" +
+            "                        \"name\":\"黑龙江\",\n" +
+            "                        \"children\":\n" +
+            "                            [\n" +
+            "                                {\"name\":\"哈尔滨\"},\n" +
+            "                                {\"name\":\"齐齐哈尔\"},\n" +
+            "                                {\"name\":\"牡丹江\"},\n" +
+            "                                {\"name\":\"大庆\"}\n" +
+            "                            ]\n" +
+            "                    },\n" +
+            "\n" +
+            "                    {\n" +
+            "                        \"name\":\"新疆\" ,\n" +
+            "                        \"children\":\n" +
+            "                            [\n" +
+            "                                {\"name\":\"乌鲁木齐\"},\n" +
+            "                                {\"name\":\"克拉玛依\"},\n" +
+            "                                {\"name\":\"吐鲁番\"},\n" +
+            "                                {\"name\":\"哈密\"}\n" +
+            "                            ]\n" +
+            "                    }\n" +
+            "                ]\n" +
+            "        }";
+        var root = JSON.parse(CityTreedata);
+        console.log(root);
+        let nodes = d3.hierarchy(root);
+        console.log(nodes);
+
+
+
 
     }
     render() {
