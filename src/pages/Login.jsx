839a552730d3711132e52e64ada52b8e73e45f1e
nykee <nykee@sina.com>
2018-04-23 17:46:02 +0800

    更新dashboard

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -6,7 +6,7 @@ class Login extends React.Component {
             <div>
                 <div className="signin">
                     <form action="">
-                        <p className="welcome">欢迎登陆逸动云</p>
+                        <p className="welcome">欢迎登陆XY云</p>
                         <input type="text" className="user" value="" placeholder="请输入用户名"  id="uname"/>
                         <input type="password" className="pass" value="" placeholder="请输入密码" id="pwd" />
                         <div  className="reg_help" style={{visibility:'hidden'}}><span>登陆建议</span></div>
--- src/pages/ManageCenter/SalesDashboard/OrderLists.jsx
+++ src/pages/ManageCenter/SalesDashboard/OrderLists.jsx
@@ -4,11 +4,67 @@ class OrderLists extends React.Component {
 
 
     render() {
+        let orders =[
+            {item:'HTC Mobile',Costumer:'alan b',price:'$438',Product:'B-TZF'},
+            {item:'Samsung Mobile',Costumer:'alan c',price:'$680',Product:'C-565'},
+            {item:'Apple Mobile',Costumer:'alan d',price:'$680',Product:'A-292'},
+            {item:'HTC Mobile',Costumer:'alan e',price:'$438',Product:'D-T23'},
+            {item:'LG Mobile',Costumer:'alan G',price:'$230',Product:'G-T24'},
+            {item:'Nokia Mobile',Costumer:'alan F',price:'$120',Product:'A-TG1'}
+
+        ];
+
+       let tbodyElement =[];
+        for (let order of orders ){
+            tbodyElement.push(
+
+                    <tr>
+                        <td>{order.item}</td>
+                        <td>{order.Costumer}</td>
+                        <td>{order.price}</td>
+                        <td>{order.Product}</td>
+                    </tr>
+
+
+
+
+
+
+
+
+
+            )
+       }
 
 
         return (
+
             <div>
-                OrderLists
+                <div>
+                    <h4>Your Orders</h4>
+                </div>
+                <table>
+                    <th>
+                        <tr>
+                            <td>Item</td>
+                        </tr>
+                    </th>
+                    <th>
+                        <tr>
+                            <td>Costumer</td>
+                        </tr>
+                    </th><th>
+                        <tr>
+                            <td>Price</td>
+                        </tr>
+                    </th><th>
+                        <tr>
+                            <td>Product</td>
+                        </tr>
+                    </th>
+                    {tbodyElement  }
+                </table>
+
             </div>
 
         )
