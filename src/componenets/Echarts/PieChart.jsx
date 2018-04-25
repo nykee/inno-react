e42e62c1adad9285e843d5eb0154cf0423802be0
nykee <nykee@sina.com>
2018-04-25 18:04:32 +0800

    update

---
+++ src/componenets/Echarts/PieChart.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+// 引入 ECharts 主模块
+import echarts from 'echarts';
+// 引入柱状图
+import  'echarts/chart/pie';
+
+class PieChart extends React.Component {
+    constructor() {
+        super();
+        this.state = {};
+    }
+
+    componentDidMount() {
+        let chart =echarts.init(document.getElementById("Pie-E"));
+        chart.setOption(this.props.chartOption);
+
+    }
+
+    render() {
+        return (
+
+                <div id="Pie-E" style={{width:822,height:220}}>
+                </div>
+
+
+
+        )
+    }
+}
+
+export default PieChart;
--- src/pages/ManageCenter/SalesDashboard/ProductLists.jsx
+++ src/pages/ManageCenter/SalesDashboard/ProductLists.jsx
@@ -2,30 +2,141 @@ import React from 'react';
 import {Row,Col} from 'antd'
 
 import ProductListsItems from './ProductListsItems'
+import PieChart from '../../../componenets/Echarts/PieChart'
+
 class ProductLists extends React.Component {
 
 
     render() {
+        var labelTop = {
+            normal : {
+                label : {
+                    show : true,
+                    position : 'center',
+                    formatter : '{b}',
+                    textStyle: {
+                        baseline : 'bottom'
+                    }
+                },
+                labelLine : {
+                    show : false
+                }
+            }
+        };
+        var labelFromatter = {
+            normal : {
+                label : {
+                    formatter : function (params){
+                        return 100 - params.value + '%'
+                    },
+                    textStyle: {
+                        baseline : 'top'
+                    }
+                }
+            },
+        }
+        var labelBottom = {
+            normal : {
+                color: '#ccc',
+                label : {
+                    show : true,
+                    position : 'center'
+                },
+                labelLine : {
+                    show : false
+                }
+            },
+            emphasis: {
+                color: 'rgba(0,0,0,0)'
+            }
+        };
+        var radius = [40, 55];
 
+        let chartOption= {
+            series : [
+                {
+                    type : 'pie',
+                    center : ['10%', '30%'],
+                    radius : radius,
+                    x: '0%', // for funnel
+                    itemStyle : labelFromatter,
+                    data : [
+                        {name:'other', value:46, itemStyle : labelBottom},
+                        {name:'GoogleMaps', value:54,itemStyle : labelTop}
+                    ]
+                },
+                {
+                    type : 'pie',
+                    center : ['30%', '30%'],
+                    radius : radius,
+                    x:'20%', // for funnel
+                    itemStyle : labelFromatter,
+                    data : [
+                        {name:'other', value:56, itemStyle : labelBottom},
+                        {name:'Facebook', value:44,itemStyle : labelTop}
+                    ]
+                },
+                {
+                    type : 'pie',
+                    center : ['50%', '30%'],
+                    radius : radius,
+                    x:'40%', // for funnel
+                    itemStyle : labelFromatter,
+                    data : [
+                        {name:'other', value:65, itemStyle : labelBottom},
+                        {name:'Youtube', value:35,itemStyle : labelTop}
+                    ]
+                },
+                {
+                    type : 'pie',
+                    center : ['70%', '30%'],
+                    radius : radius,
+                    x:'60%', // for funnel
+                    itemStyle : labelFromatter,
+                    data : [
+                        {name:'other', value:70, itemStyle : labelBottom},
+                        {name:'Google+', value:30,itemStyle : labelTop}
+                    ]
+                },
+                {
+                    type : 'pie',
+                    center : ['90%', '30%'],
+                    radius : radius,
+                    x:'80%', // for funnel
+                    itemStyle : labelFromatter,
+                    data : [
+                        {name:'other', value:73, itemStyle : labelBottom},
+                        {name:'Weixin', value:27,itemStyle : labelTop}
+                    ]
+                },
+
+            ]
+        };
 
         return (
-            <Row>
-                <Col span={4} >
-                    <ProductListsItems name="HuaWei" qty="1900"/>
-                </Col>
-                <Col span={4} offset={1}>
-                     <ProductListsItems name="HTC" qty="852"/>
-                </Col>
-                <Col span={4} offset={1}>
-                    <ProductListsItems name="SamSung" qty="121"/>
-                </Col>
-                <Col span={4} offset={1}>
-                    <ProductListsItems name="Apple" qty="2400"/>
-                </Col>
-                <Col span={4} offset={1}>
-                    <ProductListsItems name="XiaoMi" qty="1800"/>
-                </Col>
-            </Row>
+            <div>
+                <Row>
+                    <Col span={4} >
+                        <ProductListsItems name="HTC" qty="450"/>
+                    </Col>
+                    <Col span={4} offset={1}>
+                        <ProductListsItems name="Nokia" qty="657"/>
+                    </Col>
+                    <Col span={4} offset={1}>
+                        <ProductListsItems name="LG" qty="1200"/>
+                    </Col>
+                    <Col span={4} offset={1}>
+                        <ProductListsItems name="Samsung" qty="1800"/>
+                    </Col>
+                    <Col span={4} offset={1}>
+                        <ProductListsItems name="Apple" qty="2400"/>
+                    </Col>
+                </Row>
+                <Row>
+                    <PieChart chartOption={chartOption} />
+                </Row>
+            </div>
+
 
         )
     }
