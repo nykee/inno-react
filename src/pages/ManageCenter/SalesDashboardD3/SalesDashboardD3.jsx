382ed15982d7d0575e3916034eaec4d9ea14e141
nykee <nykee@sina.com>
2018-06-04 17:45:14 +0800

    更新serverdashboard

--- src/pages/ManageCenter/SalesDashboardD3/SalesDashboardD3.jsx
+++ src/pages/ManageCenter/SalesDashboardD3/SalesDashboardD3.jsx
@@ -15,26 +15,26 @@ class SalesDashboardD3 extends React.Component {
         return (
             <div style={{color:'#fff',paddingLeft:'0.5rem'}}>
                 <Row gutter={2}>
-                    <Col span={17}>
+                    <Col xxl={{span:17}} lg={{span:17}}  md={{span:24}} sm={{span:24}} xs={{span:24}} style={{marginTop:'.8rem'}}>
                         <MonthlySummary/>
                     </Col>
-                    <Col span={7}>
+                    <Col xxl={{span:7}} lg={{span:7}}  md={{span:24}} sm={{span:24}} xs={{span:24}} style={{marginTop:'.8rem'}}>
                         <PercentCount/>
                     </Col>
                 </Row>
                 <Row gutter={2} style={{marginTop:'.8rem'}}>
-                    <Col span={17}>
+                    <Col xxl={{span:17}} lg={{span:17}}  md={{span:24}} sm={{span:24}} xs={{span:24}} style={{marginTop:'.8rem'}}>
                         <OrderLists/>
                     </Col>
-                    <Col span={7}>
+                    <Col xxl={{span:7}} lg={{span:7}}  md={{span:24}} sm={{span:24}} xs={{span:24}} style={{marginTop:'.8rem'}}>
                         <AmountCount/>
                     </Col>
                 </Row>
                 <Row gutter={2} style={{marginTop:'.8rem'}}>
-                    <Col span={17}>
+                    <Col xxl={{span:17}} lg={{span:17}}  md={{span:24}} sm={{span:24}} xs={{span:24}} style={{marginTop:'.8rem'}}>
                         <ProductLists/>
                     </Col>
-                    <Col span={7}>
+                    <Col xxl={{span:7}} lg={{span:7}}  md={{span:24}} sm={{span:24}} xs={{span:24}} style={{marginTop:'.8rem'}}>
                         <SellersCount/>
                     </Col>
                 </Row>
