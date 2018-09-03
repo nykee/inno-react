670cae6b29805118a0b3ceba4eb83c3a0065bf84
nykee <nykee@sina.com>
2018-09-03 17:34:10 +0800

    update

--- src/chartConfig/commonEchartsConifg.js
+++ src/chartConfig/commonEchartsConifg.js
@@ -62,4 +62,61 @@ let pieChartOption ={
     ]
 };
 
-export  {pieChartOption};
+
+
+let dataFormatter=function (params) {
+    return params.data.value
+};
+var labelTop = {
+    normal : {
+        color:'#F5AE46',
+        label : {
+            show : true,
+            position : 'center',
+            formatter : dataFormatter,
+            textStyle: {
+                baseline : 'bottom'
+            }
+        },
+        labelLine : {
+            show : false
+        }
+    }
+};
+
+var labelBottom = {
+    normal : {
+        color: '#1F242A',
+        label : {
+            show : false,
+            position : 'center'
+        },
+        labelLine : {
+            show : false
+        }
+    },
+    emphasis: {
+        color: 'rgba(0,0,0,0)'
+    }
+};
+var radius = [40, 55];
+
+
+let commonDPieOption={
+    series : [
+        {
+            type : 'pie',
+            center : ['50%', '50%'],
+            radius : radius,
+            x: '0%', // for funnel
+            // itemStyle : labelFromatter,
+            data : [
+                {name:'other', value:550, itemStyle : labelBottom},
+                {name:'HTC', value:450,itemStyle : labelTop}
+            ]
+        },
+
+    ]
+};
+
+export  {pieChartOption,commonDPieOption};
--- src/pages/ManageCenter/TransDashboard/AllSummaryPie.jsx
+++ src/pages/ManageCenter/TransDashboard/AllSummaryPie.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PieChart from "../../../componenets/Echarts/PieChart";
+import commonDPieOption from '../../../chartConfig/commonEchartsConifg'
 
 class AllSummaryPie extends React.Component {
     constructor() {
@@ -12,60 +13,8 @@ class AllSummaryPie extends React.Component {
     }
 
     render() {
-        let dataFormatter=function (params) {
-            return params.data.value
-        };
-        var labelTop = {
-            normal : {
-                color:'#F5AE46',
-                label : {
-                    show : true,
-                    position : 'center',
-                    formatter : dataFormatter,
-                    textStyle: {
-                        baseline : 'bottom'
-                    }
-                },
-                labelLine : {
-                    show : false
-                }
-            }
-        };
+        let ChartOption = commonDPieOption;
 
-        var labelBottom = {
-            normal : {
-                color: '#1F242A',
-                label : {
-                    show : false,
-                    position : 'center'
-                },
-                labelLine : {
-                    show : false
-                }
-            },
-            emphasis: {
-                color: 'rgba(0,0,0,0)'
-            }
-        };
-        var radius = [40, 55];
-
-
-        let ChartOption={
-            series : [
-                {
-                    type : 'pie',
-                    center : ['50%', '50%'],
-                    radius : radius,
-                    x: '0%', // for funnel
-                    // itemStyle : labelFromatter,
-                    data : [
-                        {name:'other', value:550, itemStyle : labelBottom},
-                        {name:'HTC', value:4550,itemStyle : labelTop}
-                    ]
-                },
-
-            ]
-        };
         return (
             <div>
                 <PieChart chartID="allSummary" chartOption={ChartOption}/>
